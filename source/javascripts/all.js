359fae41de8ca323ca3db7b407a966367ea79d52
Karthik Sirasanagandla <sirkarthik@gmail.com>
2014-10-10 18:53:14 +0530

    Reduce n/w roundtrip - Middleman AssetPipeline - Deploying combined JS only

---
+++ source/javascripts/all.js
@@ -0,0 +1,22 @@
+//WARNING: DO NOT CHANGE THE ORDER Of IMPORTANT
+//NOTE: Order of require is important for dependency management
+
+//= require "_modernizr.custom"
+//= require "_jquery-1.10.2.min"
+//= require "_bootstrap.min"
+//= require "_classie"
+//= require "_cbpAnimatedHeader.min"
+//= require "_owl.carousel.min"
+//= require "_scrollReveal"
+//= require "_jquery.scrollTo"
+//= require "_jquery.nav"
+//= require "_imagesloaded.pkgd.min"
+//= require "_isotope.min"
+//= require "_jquery.magnific-popup.min"
+//= require "_jqBootstrapValidation"
+//= require "_custom"
+
+//The files below aren't used at the momment
+//= require "_jquery.flexslider-min"
+//= require "_jquery.mb.YTPlayer"
+//= require "_less-1.6.1.min"
